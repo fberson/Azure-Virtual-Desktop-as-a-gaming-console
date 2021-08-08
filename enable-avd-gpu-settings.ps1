c6f8d522582f4907e0542f2393946a4ab96a4f44
Freek Berson <freek.berson@wortell.nl>
2021-08-08 09:43:30 +0200

    Delete enable-avd-gpu-settings.ps1

--- enable-avd-gpu-settings.ps1
+++
@@ -1,4 +0,0 @@
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name 'bEnumerateHWBeforeSW' -Value 1  -PropertyType 'DWORD'
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name 'AVCHardwareEncodePreferred' -Value 1  -PropertyType 'DWORD'
-
-
